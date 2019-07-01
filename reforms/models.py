ff307ae7efc58ce4295fad9fa4fcf38e219263a4
James Turk <dev@jamesturk.net>
2019-07-01 13:19:04 -0400

    allow blanks in editing

--- reforms/models.py
+++ reforms/models.py
@@ -6,15 +6,15 @@ class State(models.Model):
     abbreviation = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=30)
 
-    draws_congressional_lines = models.CharField(max_length=300)
-    draws_state_lines = models.CharField(max_length=300)
-    legislative_control = models.CharField(max_length=300)
+    draws_congressional_lines = models.CharField(max_length=300, blank=True)
+    draws_state_lines = models.CharField(max_length=300, blank=True)
+    legislative_control = models.CharField(max_length=300, blank=True)
 
-    process = MarkupField(markup_type="markdown")
-    issues = MarkupField(markup_type="markdown")
-    actions = MarkupField(markup_type="markdown")
-    pitfalls = MarkupField(markup_type="markdown")
-    contacts = MarkupField(markup_type="markdown")
+    process = MarkupField(markup_type="markdown", blank=True)
+    issues = MarkupField(markup_type="markdown", blank=True)
+    actions = MarkupField(markup_type="markdown", blank=True)
+    pitfalls = MarkupField(markup_type="markdown", blank=True)
+    contacts = MarkupField(markup_type="markdown", blank=True)
 
     last_updated = models.DateTimeField(auto_now=True)
 
