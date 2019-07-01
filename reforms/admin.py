9f84b9f99d5c4e17c5313f791c32bfba19145876
James Turk <dev@jamesturk.net>
2019-07-01 15:44:18 -0400

    view comments in admin

--- reforms/admin.py
+++ reforms/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import State
+from .models import State, Comment
 
 @admin.register(State)
 class StateAdmin(admin.ModelAdmin):
     readonly_fields = ("name", "abbreviation")
     ordering = ("name",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "from_email", "state", "created")
+    readonly_fields = ("comment", "name", "from_email", "state")
+    list_filter = ("state",)
+    ordering = ("created",)
